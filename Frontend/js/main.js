88d263b86ef253c043ab9557713703e042f0ee24
GunelHus <guneloh@code.edu.az>
2019-08-15 18:51:32 +0400

    first owl added

---
+++ Frontend/js/main.js
@@ -0,0 +1,21 @@
+$('.slide').owlCarousel({
+    loop: true,
+    dots: false,
+    margin: 10,
+    nav: false,
+    navSpeed: 800,
+    autoplay: false,
+     transitionStyle: "fade",
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 1
+        },
+        992: {
+            items: 1
+        }
+    }
+
+})
