d19c5c7756e80f2bc7e4c0e42b28818635bd9287
GunelHus <guneloh@code.edu.az>
2019-08-16 17:13:40 +0400

    second carousel added

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -1,10 +1,10 @@
 $('.slide').owlCarousel({
     loop: true,
     dots: false,
-    margin: 10,
+    // margin: 10,
     nav: false,
     navSpeed: 800,
-    autoplay: false,
+    autoplay: true,
      transitionStyle: "fade",
     responsive: {
         0: {
@@ -20,16 +20,38 @@ $('.slide').owlCarousel({
 
 })
 
+$('.slide2').owlCarousel({
+    loop: true,
+    dots: false,
+    margin: 10,
+    nav: false,
+    navSpeed: 800,
+    autoplay: true,
+     transitionStyle: "fade",
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 1
+        },
+        992: {
+            items: 3
+        }
+    }
+
+}) 
 let acc = document.getElementsByClassName("btn-accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+    this.classList.toggle("color-change");
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
+        
       panel.style.display = "block";
     }
   });
