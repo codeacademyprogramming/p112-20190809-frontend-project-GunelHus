85e8bc54ab239df27c65ec07fda5cb2a1f8519d8
GunelHus <guneloh@code.edu.az>
2019-08-16 11:29:02 +0400

    video and accordion sections added

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -19,3 +19,18 @@ $('.slide').owlCarousel({
     }
 
 })
+
+let acc = document.getElementsByClassName("btn-accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
